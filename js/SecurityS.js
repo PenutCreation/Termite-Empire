4300fce384ed1365bcaf785c5f4ccdb50bb32ff8
OniwaPenut <137640432+PenutCreation@users.noreply.github.com>
2024-12-01 13:50:06 +0800

    Update SecurityS.js

--- js/SecurityS.js
+++ js/SecurityS.js
@@ -1,13 +1,1 @@
-function checkBrowser() {
-            var userAgent = navigator.userAgent.toLowerCase();
-
-            // Check if the browser is Chrome (and not Edge or other Chromium-based browsers)
-            if (userAgent.indexOf('chrome') === -1 || userAgent.indexOf('edge') !== -1 || userAgent.indexOf('opr') !== -1 || userAgent.indexOf('safari') !== -1) {
-                // If it's not Chrome, show an alert or redirect the user
-                alert("Warning:You Can Only Play on chrome")
-                // Optionally, you can redirect:
-                window.location.href = "https://www.google.com/chrome/";
-            }
-        }
-
-        window.onload = checkBrowser;
+alert("User Verified!")
